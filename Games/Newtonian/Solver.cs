f0cc5f77c2dc5bfe5bab48c88683d01e78edc5b9
Matthew Healy <mhrh3@mst.edu>
2018-11-10 16:26:48 -0600

    manage inefficiently

--- Games/Newtonian/Solver.cs
+++ Games/Newtonian/Solver.cs
@@ -29,23 +29,37 @@ namespace Joueur.cs.Games.Newtonian
 
         public static void ManagerWork(Unit u)
         {
-            if (!Rules.IsFull(u))
+            var goalType = AI.PLAYER.Heat < AI.PLAYER.Pressure && AI.GAME.Tiles.Where(t => Extensions.GetAmount(t, AI.REDIUM) > 0).Count() > 0 ? AI.REDIUM : AI.BLUEIUM;
+            if (Rules.OpenCapacity(u) > 0)
             {
-                var goalType = null; // which type of oren't we need
                 var goalTiles = AI.GAME.Tiles.Where(t => Extensions.GetAmount(t, goalType) > 0).ToHashSet();
                 var path = GetPath(u.ToPoint().Singular(), (p => goalTiles.Contains(p.ToTile()))).ToArray();
                 foreach(Point step in path.Skip(1).SkipLast(1).Take(u.Moves))
                 {
                     u.Move(step.ToTile());
                 }
-                if(Extensions.GetAmount(u.Tile, goalType) > 0)
+                var pickup = path.Last().ToTile();
+                var currentAmt = goalType == AI.REDIUM ? u.Redium : u.Blueium;
+                var pickupAmt = AI.MANAGER.CarryLimit - currentAmt - (goalType == AI.REDIUM ? pickup.Redium : pickup.Blueium);
+                if (u.Tile == pickup || u.Tile.HasNeighbor(pickup))
                 {
-                    u.Pickup(u.Tile)
+                    u.Pickup(pickup, pickupAmt, goalType);
                 }
-
-                if (u.Acted)
+            }
+            else
+            {
+                var goalTiles = AI.GAME.Tiles.Where(t => t.Type == "generator").ToHashSet();
+                var path = GetPath(u.ToPoint().Singular(), (p => goalTiles.Contains(p.ToTile()))).ToArray();
+                foreach(Point step in path.Skip(1).SkipLast(1).Take(u.Moves))
                 {
-                    
+                    u.Move(step.ToTile());
+                }
+                var jenny = path.Last().ToTile();
+                if(u.Tile == jenny || u.Tile.HasNeighbor(jenny))
+                {
+                    u.Drop(jenny, AI.MANAGER.CarryLimit, AI.REDIUM);
+                    u.Drop(jenny, AI.MANAGER.CarryLimit, AI.BLUEIUM);
+
                 }
             }
         }
